f312ae155015b0b0f19d37bccd7c635361ed59f5
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-08-09 18:40:27 +0530

    Form Validation Added using Formik and Yup

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
-export default function ProfileDetails({ firstName, lastName, email, profilePic, handleFirstNameChange, handleLastNameChange, handleEmailChange, handleProfilePicChange, handleProfileSave }) {
+export default function ProfileDetails({ fullname, email, profilePic, handleNameChange, handleEmailChange, handleProfilePicChange, handleProfileSave }) {
+   // Validation schema for Formik
+const validationSchema = Yup.object({
+  fullname: Yup.string().required('Name is a required field'),
+  email: Yup.string().required('Email is required, it helps people reach you through mail')
+});
+  
+ // -----------
+
+const formik = useFormik({
+  initialValues: {
+    fullname: '',
+    email: '',
+  },
+  validationSchema,
+  onSubmit: (values) => {
+    handleProfileSave(values);
+  },
+});
+
+
+ // Handle select changes
+ const handleNameFieldInput = (e) => {
+  formik.handleChange(e);
+  handleNameChange(e); // Call the prop function
+};
+
+// Handle link input changes
+const handleEmailFieldInput = (e) => {
+  formik.handleChange(e);
+  handleEmailChange(e); // Call the prop function
+};
   return (
+    <form onSubmit={formik.handleSubmit}>
     <div>
       <h3 className='texttheme'>Add Your Profile Picture And Email</h3>
       <h5 className='texttheme'>Add your details and share with the world</h5>
@@ -21,27 +55,22 @@ export default function ProfileDetails({ firstName, lastName, email, profilePic,
 
       <div className='inpField'>
         <label>
-          First Name
+          Name
         </label>
         <input
           type="text"
-          value={firstName}
-          onChange={handleFirstNameChange}
+          value={fullname}
+          onChange={handleNameFieldInput}
+          name="fullname"
           placeholder="Enter your first name"
         />
+          {/* {!firstName && <p>field required</p>} */}
+          {formik.touched.fullname && formik.errors.fullname ? (
+              <div className="error">{formik.errors.fullname}</div>
+            ) : null}
+            
       </div>
 
-      <div className='inpField'>
-        <label>
-          Last Name
-        </label>
-        <input
-          type="text"
-          value={lastName}
-          onChange={handleLastNameChange}
-          placeholder="Enter your last name"
-        />
-      </div>
 
       <div className='inpField'>
         <label>
@@ -50,17 +79,24 @@ export default function ProfileDetails({ firstName, lastName, email, profilePic,
         <input
           type="text"
           value={email}
-          onChange={handleEmailChange}
+          onChange={handleEmailFieldInput}
+           name="email"
           placeholder="Enter your email"
         />
+        {/* {!email && <p>field required</p>} */}
+        {formik.touched.email && formik.errors.email ? (
+              <div className="error">{formik.errors.email}</div>
+            ) : null}
+            
       </div>
       
       <div className="link-card-footer">
-        <button className='btnbordered btngreen' onClick={handleProfileSave}>
+        <button className='btnbordered btngreen' type="submit" disabled={!formik.isValid}>
           <FontAwesomeIcon icon={faCheckCircle} /> Save
         </button>
       </div>
     </div>
+    </form>
   );
 };
 
