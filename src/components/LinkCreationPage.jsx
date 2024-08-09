1f1cb93b425b8048f0d1b7d2bac0dca56665cb01
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-08-09 18:38:44 +0530

    Update LinkCreationPage.jsx

--- src/components/LinkCreationPage.jsx
+++ src/components/LinkCreationPage.jsx
@@ -4,12 +4,13 @@ import { faLink } from '@fortawesome/free-solid-svg-icons';
 import SavedLinks from './SavedLinks';
 
 
-export default function LinkCreationPage({ addLink, addNewLink, handleLinkRemove, handleSaveLink, handleLink, handleSocial, linkBucket, handleUpdateLink }) {
+export default function LinkCreationPage({ addLink, addNewLink, handleLinkRemove, handleSaveLink, handleLink, handleSocial, linkBucket, handleUpdateLink, handleDeleteLink }) {
 
   return (
     <div className='customlinks'>
       <h3 className='texttheme'>Customise Your Links</h3>
       <h5 className='texttheme'>Add, edit, or remove links below and share all your profiles with the world.</h5>
+      <div className='m-1'>
       {!addNewLink && (
         <button
           className='btnbordered w-100'
@@ -21,7 +22,7 @@ export default function LinkCreationPage({ addLink, addNewLink, handleLinkRemove
       )}
 
       {!addNewLink &&
-        <SavedLinks linkBucket={linkBucket} handleUpdateLink={handleUpdateLink} />
+        <SavedLinks linkBucket={linkBucket} handleUpdateLink={handleUpdateLink} handleDeleteLink={handleDeleteLink} />
       }
 
       {addNewLink && (
@@ -32,6 +33,7 @@ export default function LinkCreationPage({ addLink, addNewLink, handleLinkRemove
           handleSocial={handleSocial}
         />
       )}
+      </div>
     </div>
   );
 }
