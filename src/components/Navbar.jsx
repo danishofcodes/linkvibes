003216d5bf98517a7b4529d5005d9c7a1e54deb3
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-08-22 17:40:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,11 @@ import { faEye, faLink, faQuestionCircle } from '@fortawesome/free-solid-svg-ico
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-export default function Navbar({ menuSelector, menu , handlePreview}) {
+export default function Navbar() {
   return (
     <nav>
       <h3 className="logo"> <FontAwesomeIcon icon={faLink} />linkvibes</h3>
-      <div>
-        <button
-          className={`btnsolid ${menu === 'links' ? 'active' : ''}`}
-          onClick={() => menuSelector('links')}
-        >
-          Links
-        </button>
-        <button
-          className={`btnsolid ${menu === 'profiledetails' ? 'active' : ''}`}
-          onClick={() => menuSelector('profiledetails')}
-        >
-          Profile Details
-        </button>
-      </div>
+     
       <div style={{color:"#1dafad"}}><FontAwesomeIcon icon={faQuestionCircle}/></div>
       {/* <button className='btn-rounded' onClick={handlePreview}> <FontAwesomeIcon icon={faEye} /> &nbsp;preview</button> */}
     </nav>
